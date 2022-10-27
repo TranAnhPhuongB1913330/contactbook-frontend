0933747cbceea7872871778811f120becbfb5a0e
Your Name <phuong0987654321@gmail.com>
2022-10-27 09:01:33 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+import NotFound from "@/views/NotFound.vue"
 const routes = [
     {
         path: "/",
         name: "contactbook",
         component: ContactBook,
+    },
+
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: NotFound,
+        // component: () => import("@/views/NotFound.vue"),
     },
 ];
 
